6bf28aecaff54fc413c82223d2c147b7d65b2533
Mikalai Kanstantsinau <konst007@tut.by>
2017-11-16 00:53:50 +0300

    3. make_dancer_move

---
+++ src/test/java/com/konst007/DancerTest.java
@@ -0,0 +1,21 @@
+package com.konst007;
+
+import junit.framework.TestCase;
+
+public class DancerTest extends TestCase {
+
+    Dancer dancer = new Dancer();
+
+    public void testMove() throws Exception {
+
+        dancer.setPosition(5);
+        dancer.move(2);
+        assertEquals(7, dancer.getPosition());
+
+        dancer.setPosition(5);
+        dancer.move(3);
+        assertEquals(2, dancer.getPosition());
+
+    }
+
+}
